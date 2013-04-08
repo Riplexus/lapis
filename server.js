a14a82436f04b35aabd0fd2c9c7a8a22bacfd19f
Riplexus <Riplexus@gmail.com>
2013-04-08 23:07:05 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var EventEmitter2 = require('eventemitter2').EventEmitter2,
+    defaultConfig = require('./config/default'),
+    util = require('util'),
+    http = require('http'),
+    Server;
+
+Server = function Server() {
+    Server.super_.call(this, {
+        wildcard: true,
+        delimiter: '/',
+        newListener: true,
+        maxListeners: 20
+    });
+
+
+};
+
+util.inherits(Server, EventEmitter2);
+
+
+var s = new Server();
+
+console.log(defaultConfig);
